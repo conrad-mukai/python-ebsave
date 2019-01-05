8685e18aa29db9af5b4e5a59bf0fd1d43c28519f
Conrad Mukai <conrad@mukai-home.net>
2019-01-05 15:42:01 -0800

    Make urllib3 spec dynamic

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ ebsave packaging
 """
 
 # system imports
+import platform
 from setuptools import setup
+
+if tuple(int(x) for x in platform.python_version().split('.')) <= (2, 7, 6):
+    urllib3_spec = 'urllib3<=1.23'
+else:
+    urllib3_spec = 'urllib3'
 
 
 # packaging
 setup(name='ebsave',
-      version='0.2.0',
+      version='0.2.2',
       author='Conrad Mukai',
       author_email='conrad@mukai-home.net',
       url='https://github.com/conrad-mukai/python-ebsave',
@@ -16,7 +22,7 @@ setup(name='ebsave',
       install_requires=[
           'boto3',
           'requests',
-          'urllib3<=1.23;python_version<="2.7.6"'
+          urllib3_spec
       ],
       entry_points={'console_scripts': ['ebsave=ebsave:main']},
       zip_safe=False
